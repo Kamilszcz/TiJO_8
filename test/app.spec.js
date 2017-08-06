445b6725ff51529b1df74f991cd8f0056eca847b
Kamilszcz <dragon1393@wp.pl>
2017-08-06 14:33:22 +0200

    Dodano przypadki testowe

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,21 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('should return reversed number', function(){
+        expect(app.reverseNumber(123)).to.eql(321);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('should return letters froom argument', function(){
+        expect(app.returnOnlyLetter('kamil123')).to.eql('kamil');
+    });
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true when argument is email', function(){
+        expect(app.isEmail('kamil123@gmail.com')).to.eql(true);
+    });
 });
